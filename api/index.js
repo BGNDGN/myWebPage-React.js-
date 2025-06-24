d808934ae49aa1b6fd19e8a1acb3dfe602cc07e3
Burak <burakgundogan25@gmail.com>
2025-06-24 21:09:08 +0300

    changed

--- api/index.js
+++ api/index.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const serverless = require('serverless-http');
 const path = require('path');
+const app = express();
 
 require('dotenv').config({ path: path.resolve(__dirname, '../server/.env') });
 
 const connectDB = require('../server/config/mongodbConnect');
 const authRoutes = require('../server/routes/authRoutes');
 
-const app = express();
-
 connectDB().catch(console.error);
+
 app.use(express.json());
 
 app.use('/api', authRoutes);
@@ -18,4 +18,5 @@ app.get('/api/health', (req, res) => {
   res.json({ status: 'API çalışıyor!' });
 });
 
+module.exports = app;
 module.exports.handler = serverless(app);
