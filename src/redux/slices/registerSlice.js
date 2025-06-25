cf65fbffb5b23f2862b2b3075b4ef5cbd0fdf5d4
Burak <burakgundogan25@gmail.com>
2025-06-26 01:44:26 +0300

    last dance 6

--- src/redux/slices/registerSlice.js
+++ src/redux/slices/registerSlice.js
@@ -1,6 +1,6 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
-import baseURL from '.././api/baseURL';
+import baseURL from '../../api/baseURL';
 
 export const registerUser = createAsyncThunk(
   'register/registerUser',
