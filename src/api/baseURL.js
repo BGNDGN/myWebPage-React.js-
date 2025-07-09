0b57ca6581c6cbb65a5e992d02120a4df3109229
Burak <burakgundogan25@gmail.com>
2025-07-10 01:11:56 +0300

    Deploy için güncelleme

--- src/api/baseURL.js
+++ src/api/baseURL.js
@@ -1,3 +1,5 @@
 const baseURL = process.env.REACT_APP_API_URL;
-console.log('API base URL:', baseURL);
+console.log('API base URL:', baseURL);  // Burada undefined olmamalı
+console.log('All env vars:', process.env);
+
 export default baseURL;
